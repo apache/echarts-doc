0f361e717100a3c6fa706f12453a1e58b60c9397
pah100 <sushuang0322@gmail.com>
2016-03-29 14:18:51 +0800

    update release script

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 basepath=$(cd `dirname $0`; pwd)
 
 node ${basepath}/build.js $1 # $1 is myname, than use configmyname.js but not config.js.
+
+rm ${basepath}/../echarts-home/documents/cn/*
 cp -R ${basepath}/test/documents/cn/ ${basepath}/../echarts-home/documents/cn/
+
+
 # asset
+rm -r ${basepath}/../echarts-home/documents/asset/*
 cp -R ${basepath}/asset/ ${basepath}/../echarts-home/documents/asset/
