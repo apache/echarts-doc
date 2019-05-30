4de98be8432405f1e1259d99a5a6e7d89c0a67c3
Ovilia <zwl.sjtu@gmail.com>
2019-05-30 11:21:00 +0800

    apache website transfer

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ basepath=$(cd `dirname $0`; pwd)
 # Build cn doc for www.echartsjs.com.
 node ${basepath}/build.js
 # Do not rm, keep option3.json.
-cp -R ${basepath}/public/cn/documents/cn/ ${basepath}/../echarts-www/documents/cn/
-
+cp -R ${basepath}/public/cn/documents/cn/ ${basepath}/../echarts-www/documents/zh/
+cp -R ${basepath}/public/en/documents/en/ ${basepath}/../incubator-echarts-website/documents/zh/
 
 # Build en doc for gf-page of echarts-doc and apache.echarts.com.
 node ${basepath}/build.js github
