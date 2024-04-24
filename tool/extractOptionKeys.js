a5d803b3b5f65d3849c6deeafd832f3527c74719
plainheart <yhen@all-my-life.cn>
2024-04-24 12:50:16 +0800

    tweak extractOptionKeys tool

--- tool/extractOptionKeys.js
+++ tool/extractOptionKeys.js
@@ -3,4 +3,7 @@ const path = require('node:path');
 const { extractOptionKeys } = require('./schemaHelper');
 
 const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../public/zh/documents/option.json'), 'utf-8'));
-console.log(JSON.stringify(extractOptionKeys(schema)));
+
+const header = `// THIS FILE IS GENERATED, DON'T MODIFY\n/* eslint-disable */`;
+
+fs.writeFileSync(`./option-keywords.js`, `${header}\nexport const keywords = ${JSON.stringify(extractOptionKeys(schema))};`);
